c8fdf3d4015442db4eb9909f107f564c9d26d7b8
Andres Campuzano <campuxano88@gmail.com>
2020-03-09 09:36:20 -0500

    First page created.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './global.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Badge from '../src/components/Badge';
+
+ReactDOM.render(
+  <Badge
+    firstName='Andrés'
+    lastName='Campuzano'
+    avatar='https://www.gravatar.com/userimage/104927907/a13b414d73c239ac7ec47d152d20f27c.jpeg'
+    jobTitle='Frontend Developer, Student'
+    twitter='campuzanoCOL'
+  />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
