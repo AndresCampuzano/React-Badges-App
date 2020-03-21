6560e032a73b5ec05845f3d28603cd5ef95eb461
Andres Campuzano <campuxano88@gmail.com>
2020-03-21 08:53:05 -0500

    Now data is provided by a fake API.

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaTwitterSquare } from 'react-icons/fa';
 
 import './styles/BadgesList.css';
 
 class BadgesList extends Component {
-    render() {
-        return (
-            <div className='BadgesList'>
-                <ul>
-                    {this.props.badges.map(badge => {
-                        return (
-                            <li key={badge.id} className='BadgesListItem'>
-                                <div className='elementsList'>
-                                    <div className='elementOne'>
-                                        <img
-                                            className='BadgesListItem__avatar'
-                                            src={badge.avatarUrl}
-                                            alt='Avatar image'
-                                        />
-                                    </div>
-                                    <div className='elementTwo'>
-                                        <p className='bold-text'>
-                                            {badge.firstName} {badge.lastName}
-                                        </p>
-                                        <a href='/'>
-                                            <FaTwitterSquare />
-                                            {badge.twitter}
-                                        </a>
-                                        <p>{badge.jobTitle}</p>
-                                    </div>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+	render() {
+		if (this.props.badges.length === 0) {
+			return (
+				<div>
+					<h3>No badges were found</h3>
+					<Link className='btn btn-primary' to='/badges/new'>
+						Create new badge
+					</Link>
+				</div>
+			);
+		}
+		return (
+			<div className='BadgesList'>
+				<ul>
+					{this.props.badges.map(badge => {
+						return (
+							<li key={badge.id} className='BadgesListItem'>
+								<div className='elementsList'>
+									<div className='elementOne'>
+										<img
+											className='BadgesListItem__avatar'
+											src={badge.avatarUrl}
+											alt='Avatar image'
+										/>
+									</div>
+									<div className='elementTwo'>
+										<p className='bold-text'>
+											{badge.firstName} {badge.lastName}
+										</p>
+										<a href='/'>
+											<FaTwitterSquare />
+											{badge.twitter}
+										</a>
+										<p>{badge.jobTitle}</p>
+									</div>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default BadgesList;
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,16 +5,36 @@ import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 
+import api from '../api';
+
 class Badges extends Component {
 	state = {
 		loading: true,
 		error: null,
 		data: undefined
 	};
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData = async () => {
+		this.setState({ loading: true, error: null });
+
+		try {
+			const data = await api.badges.list();
+			this.setState({ loading: false, data: data });
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
+	};
+
 	render() {
 		if (this.state.loading === true) {
-			return 'Loading...';
-		} else {
+			return '🕢Loading🕑...';
+		}
+		if (this.state.error) {
+			return `Error: ${this.state.error.message}`;
 		}
 
 		return (
